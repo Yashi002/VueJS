f8256ceb88c98d182e91770a161f1155118847cb
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-08-18 19:57:52 +0530

    Create compositionapi.vue

---
+++ Module/compositionapi.vue
@@ -0,0 +1,32 @@
+export default {
+  components: { RepositoriesFilters, RepositoriesSortBy, RepositoriesList },
+  props: {
+    user: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      repositories: [], // 1
+      filters: { ... }, // 3
+      searchQuery: '' // 2
+    }
+  },
+  computed: {
+    filteredRepositories () { ... }, // 3
+    repositoriesMatchingSearchQuery () { ... }, // 2
+  },
+  watch: {
+    user: 'getUserRepositories' // 1
+  },
+  methods: {
+    getUserRepositories () {
+      // using `this.user` to fetch user repositories
+    }, // 1
+    updateFilters () { ... }, // 3
+  },
+  mounted () {
+    this.getUserRepositories() // 1
+  }
+}
