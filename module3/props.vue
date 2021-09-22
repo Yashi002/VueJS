06234970fee0d6d4cc59b3c9ae1a624b1ee717ad
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-09-22 19:02:11 +0530

    Update props.vue

--- module3/props.vue
+++ module3/props.vue
@@ -2,8 +2,7 @@
     <div>
         <h1>propes</h1>
         <p>{{name}}</p>
-        <!-- <p>{{arydata[0].name}}</p> -->
-        <!-- <p>{{arydata.name}}</p> -->
+        
         <p v-for=" data in arydata" :key="data.name">{{data.name}}--{{data.lname}}</p>
     </div>
 </template>
