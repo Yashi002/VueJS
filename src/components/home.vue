9d7d75f44430aec5379370b6952b0fb153e43918
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-09-28 16:07:57 +0530

    Add files via upload

---
+++ src/components/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1 v-if="show">Conditional Rendering</h1>
+        <!-- <h1 v-else>else side</h1> -->
+        <button v-on:click="display">toggle</button>
+    
+    </div>
+</template>
+
+<script>
+
+
+export default{
+    name:'home',
+    props: {
+        data : String,
+        msg : String
+    }, data()
+    {
+        return {
+            show:false
+        }
+    },
+    methods:{
+        display()
+        {
+            this.show= true
+        }
+    }
+    }
+    
+
+</script>
+
